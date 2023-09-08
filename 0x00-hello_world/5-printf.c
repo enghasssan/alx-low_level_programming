937eda090afb392e2f3f16edb9d90974b88d1c90
enghasssan <2200146@eng.asu.edu.eg>
2023-09-08 04:51:02 +0300

    printf doesnt have \n included like puts

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * This is a code to print somthing not important at any measures
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
