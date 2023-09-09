70f25311f0ae5c056eea5b21b09ba79565645ce8
enghasssan <2200146@eng.asu.edu.eg>
2023-09-09 03:35:26 +0300

    put char needs ascii so put 0 between single quotes that represent 48 in ascii + the num so that putchar starts countring from 48 in the ascii and outputs what we need,so puchar needs ascii inside for numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print from 0 to 10 using putchar
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num;
+	
+	for (num = 0; num < 10; num++)
+		putchar(num + '0');
+	putchar('\n');
+	return (0);
+}
