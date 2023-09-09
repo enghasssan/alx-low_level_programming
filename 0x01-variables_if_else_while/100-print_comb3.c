36fb1344d60d5ea3435d76261f3927ff0e53a2c3
enghasssan <2200146@eng.asu.edu.eg>
2023-09-09 05:43:18 +0300

    print combinations of 2 diffrent numbers, only allow the smaller combination of the same 2 numbers forming that combination, used nested while loops and nested if loops

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints all comb(s) of 2 digits
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num1 = 0;
+	int num2 = 0;
+
+	while (num1 <= 9)
+	{
+		while (num2 <= 9)
+		{
+			if ( num1 != num2 && num1 < num2)
+			{
+				putchar(num1 + 48);
+				putchar(num2 + 48);
+				
+				if (num1 + num2 != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			num2++;
+		}
+		num1++;
+	}
+	putchar('\n');
+	
+	return (0);
+}
