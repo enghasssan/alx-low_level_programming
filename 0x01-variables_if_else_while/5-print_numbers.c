c89dcb9c4480ac217137d41fdad715eb238a858a
enghasssan <2200146@eng.asu.edu.eg>
2023-09-09 03:10:50 +0300

    printf

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints all from 0 to 10
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int number;
+	for (number = 0; number > 10; number++)
+		printf("%d", number);
+	return (0);
+}
