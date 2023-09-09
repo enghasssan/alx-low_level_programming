af17b4bae634394deeaffec1e91113e4fb7db5ca
enghasssan <2200146@eng.asu.edu.eg>
2023-09-09 05:07:21 +0300

    wtf

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,17 +10,17 @@
 int main(void)
 {
 	int digit = 0;
-	
-	while (digit <=9)
+
+	while (digit <= 9)
 	{
 		putchar(digit + 48);
 
-		if (digit !=9)
+		if (digit != 9)
 		{
 			putchar(',');
 			putchar(' ');
 		}
 		digit++;
 	}
-	return(0);
+	return (0);
 }
