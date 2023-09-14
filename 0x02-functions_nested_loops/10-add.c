2922e2a57e0fdd3941e905e0399da9e46bd47c90
enghasssan <2200146@eng.asu.edu.eg>
2023-09-15 00:00:32 +0300

    simple 2 inputs

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integers then return the result
+ * 
+ * @a integer input
+ *
+ * @b integer input
+ *
+ * Return: The result (success)
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
