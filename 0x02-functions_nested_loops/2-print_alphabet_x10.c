b895d410ea16a3be71762a017d2e11c571828944
enghasssan <2200146@eng.asu.edu.eg>
2023-09-14 04:29:44 +0300

    C functions and headers lvl 0.5

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function that will print the alphabet 10 times
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int ch;
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (ch = 97; ch <= 122; ch++)
+		{
+			_putchar(ch);
+		}
+	_putchar('\n');
+	}
+}
