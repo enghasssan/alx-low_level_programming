fe3c06d14dbdc4626df6912eb34036df1b4ec6e9
enghasssan <2200146@eng.asu.edu.eg>
2023-09-14 05:14:13 +0300

    check alphabetic

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * _isalpha: checks for alphabetic character upper/lower
+ * _isalpha - checks for alphabetic character upper/lower
  *
  * @c: takes input from other fns
  *
