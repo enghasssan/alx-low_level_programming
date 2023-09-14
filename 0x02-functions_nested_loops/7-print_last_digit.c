1e63595f43d1696a0147c682243092f77a0e47b1
enghasssan <2200146@eng.asu.edu.eg>
2023-09-14 19:38:42 +0300

    n % 10

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,8 +12,12 @@ int print_last_digit(int n)
 {
 	if (n < 0)
 	{
-		n = n * (-1);
+		_putchar((-1 * (n % 10)) + '0');
+		return (-1 * (n % 10));
 	}
+	else
+	{
 	_putchar((n % 10) + '0');
 	return (n % 10);
+	}
 }
