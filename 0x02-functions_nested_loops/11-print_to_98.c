8078f2b72688719677d7ed309bcfcd8d32baaebf
enghasssan <2200146@eng.asu.edu.eg>
2023-09-15 01:08:34 +0300

    printf comes to save hassan from putchar

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints all N numbers from n to 98 then \n.
+ *
+ * @n: integer input
+ *
+ * Returns: Always 0 (success)
+ */
+
+void print_to_98(int n)
+{
+	for (n = 0; n <= 98; n++)
+	{
+		printf("%d",n);
+		while (n != 98)
+		{
+		printf(", ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
