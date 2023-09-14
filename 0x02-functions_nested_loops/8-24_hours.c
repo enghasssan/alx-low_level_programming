5f2b267ddba14cac93b76b3efd0d2a289bdce205
enghasssan <2200146@eng.asu.edu.eg>
2023-09-14 22:28:08 +0300

    important function

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of jack bauer
+ * starting from 00:00 to 23:59
+ *
+ * Return: Always 0 (success)
+ */
+
+void jack_bauer(void)
+{
+	int min;
+	int hr;
+
+	for (hr = 0; hr <= 23; hr++)
+	{
+		for(min = 0; min <= 59; min++)
+		{
+			_putchar((hr / 10) + 48);
+			_putchar((hr % 10) + 48);
+			_putchar(':');
+			_putchar((min / 10) + 48);
+			_putchar((min % 10) + 48);
+			_putchar('\n');
+		}
+ 	}
+}
