0cae9c91c58dbaf81c7d22764eceee56a8d10614
enghasssan <2200146@eng.asu.edu.eg>
2023-09-14 22:58:45 +0300

    times table fn

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,14 +11,27 @@ void times_table(void)
 {
 	int i;
 	int j;
+	int prod;
 
 	for (i = 0; i <= 9; i++)
 	{
+		_putchar(48);
 		for (j = 0; j <= 9; j++)
 		{
-			_putchar((i * j) + 48);
 			_putchar(',');
 			_putchar(' ');
+			
+			prod = i * j;
+
+			if (prod <= 9)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((prod / 10) + 48);
+			}
+			_putchar((prod % 10) + 48);
 		}
 		_putchar('\n');
 	}
