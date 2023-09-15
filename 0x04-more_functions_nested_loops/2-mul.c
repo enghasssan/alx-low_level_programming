6561a72e94baa2e34ce96d92dd82fb77cc7a2c7f
enghasssan <2200146@eng.asu.edu.eg>
2023-09-16 01:16:51 +0300

    simple

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiply 2 integers
+ *
+ * @a: variable number
+ * @b: variable number
+ *
+ * Return: Product
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
