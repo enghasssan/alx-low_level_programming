08f7c77270fbe800da46ba05ef0f9cab819562b9
enghasssan <2200146@eng.asu.edu.eg>
2023-09-16 05:34:32 +0300

    i hate triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a right-angled triangle
+ *
+ * @size: input variable of height of triangle
+ *
+ * Return: Void 0
+ */
+
+void print_triangle(int size)
+{
+	int i;
+	int j;
+	
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{	
+			for (j = size - i; j > 0; j--)
+			{
+				_putchar(' ');
+			}
+			for (k = 1; k <= i; k++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
+
