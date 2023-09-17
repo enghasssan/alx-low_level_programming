268555d1ba553c145f2db7323ca64bd1ec2b25dc
enghasssan <2200146@eng.asu.edu.eg>
2023-09-17 20:13:50 +0300

    easy advanced

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: computes and prints the sum of all the multiples of 3 or 5 below 1024[
+ * Description: gets the sum of all the multiples of 3 or 5 below 1024[
  *
  * Return: Always 0 success
  */
