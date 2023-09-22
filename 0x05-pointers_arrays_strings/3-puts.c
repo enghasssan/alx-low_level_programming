4bbab39fa8f4dbe861f496d77b16bb436845f8e6
enghasssan <2200146@eng.asu.edu.eg>
2023-09-22 15:35:36 +0300

    prints string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -10,10 +10,10 @@
 
 void _puts(char *str)
 {
-	int i;
-
-	for (i = 0; *str != '\0'; i++)
+	while (*str != '\0')
 	{
-		_putchar('*str');
+		_putchar(*str + '0');
+		str++;
 	}
+	_putchar('\n');
 }
