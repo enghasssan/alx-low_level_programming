86576b3702f6e57d2fb8af51f90515692dfe4bba
enghasssan <2200146@eng.asu.edu.eg>
2023-09-22 23:02:26 +0300

    hardest in 0x05 main

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -14,12 +14,18 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int i = -1;
+	int i, len;
 
-	while (src[i] != '\0')
+	len = 0;
+
+	while (src[len] != '\0')
 	{
-		i++;
+		len++;
+	}
+	for (i = 0; i < len; i++)
+	{
 		dest[i] = src[i];
 	}
+	dest[i] = '\0';
 	return (dest);
 }
