0b63b0b52645c96cf12fa6a32197193ef618e0b6
enghasssan <2200146@eng.asu.edu.eg>
2023-09-23 02:59:57 +0300

    welding strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * *_strcat - fn concatenates(weld) two strings
+ *
+ * @dest: home string to join to
+ * @src: string to join dest away from home
+ *
+ * Return: 0 always.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	/*so we need to delete /0 of dest and then add src and then leave /0 at the end of src*/
+	int i = 0;
+	int k;
+	char temp1[100000];
+
+	while (dest[i] != '\0')
+	{
+		temp1[i] = dest[i];
+		i++;
+	}
+	k = i;
+	/*i now = num of ch in dest + 1 just need to add src*/
+	i = 0;
+	while (src[i] != '\0')
+	{
+		temp1[k] = src[i];
+		i++;
+		k++;
+	}
+	temp[i] = '\0';
+	dest = temp1;
+	return (dest);
+}
