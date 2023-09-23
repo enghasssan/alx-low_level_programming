57593e6f44d35986d05297994a78f982c9a06e1d
enghasssan <2200146@eng.asu.edu.eg>
2023-09-23 19:10:35 +0300

    _strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _strcmp - compares 2 strings.
+ * _strcmp - compares 2 strings
  *
  * @s1: string 1 of chars
  * @s2: string 2 of chars
  *
  * Return: 0 if strings are same
- * Return: -1 if s1 < s2
- * Return: 1 if s1 > s2
+ * -1 if s1 < s2
+ *  1 if s1 > s2
  */
 int _strcmp(char *s1, char *s2)
 {
@@ -35,8 +35,5 @@ int _strcmp(char *s1, char *s2)
 	{
 		return (-1);
 	}
-	else
-	{
-		return (0);
-	}
+	return (0);
 }
