1c7faacde43d3459e7654e43d98d093048e58a4d
enghasssan <2200146@eng.asu.edu.eg>
2023-09-23 05:38:26 +0300

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_strncpy - fn copies a string
+ * @dest: dest string
+ * @src: src string
+ * @n: max bytes needed from src
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		i++;
+	}
+	for (k = 0; k <= i; k++)
+	{
+		while (k <= n)
+			dest[k] = src[k];
+	}
+	return (dest);
+}
