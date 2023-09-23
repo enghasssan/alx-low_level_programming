05f6247d90612c0ac809dbc01e87e3878b011f6b
enghasssan <2200146@eng.asu.edu.eg>
2023-09-23 03:02:54 +0300

    welding strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,7 +11,6 @@
 
 char *_strcat(char *dest, char *src)
 {
-	/*so we need to delete /0 of dest and then add src and then leave /0 at the end of src*/
 	int i = 0;
 	int k;
 	char temp1[50];
@@ -21,9 +20,10 @@ char *_strcat(char *dest, char *src)
 		temp1[i] = dest[i];
 		i++;
 	}
+
 	k = i;
-	/*i now = num of ch in dest + 1 just need to add src*/
 	i = 0;
+
 	while (src[i] != '\0')
 	{
 		temp1[k] = src[i];
