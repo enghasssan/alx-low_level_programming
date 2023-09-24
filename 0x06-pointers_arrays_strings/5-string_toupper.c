529355fb6f21b3b6118841ef00e9526a62112527
enghasssan <2200146@eng.asu.edu.eg>
2023-09-25 02:26:35 +0300

    exchange string lower to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *string_toupper - fn subs. lowercase char by
+ * uppercase chars of string
+ * @a: string
+ * Return: string
+ */
+
+char *string_toupper(char *a)
+{
+	int i = 0;
+
+	while (a[i] != '\0')
+	{
+		if (*(a + i) >= 'a' && *(a + i) <= 'z')
+		{
+			*(a + 1) = *(a + 1) - 32;
+		}
+		i++;
+	}
+	return (a);
+}
