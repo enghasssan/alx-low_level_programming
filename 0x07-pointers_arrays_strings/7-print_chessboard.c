70a3f88a6f04a13c09abfbb0e811bfc760b37b6a
enghasssan <2200146@eng.asu.edu.eg>
2023-09-28 03:50:47 +0300

    chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_chessboard - fun prints the chessboard
+ * @a: 2D array of 8 x 8
+ * Return: always 0
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8 i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+}
