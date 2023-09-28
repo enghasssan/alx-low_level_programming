368754027cbd454547d27692afa2f75c5a749a45
enghasssan <2200146@eng.asu.edu.eg>
2023-09-28 03:39:42 +0300

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * *_strstr - locates a substring
+ * @haystack: string
+ * @needle: substring
+ * Return: pointer to beginning of substring
+ * else NULL
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	for (;haystack != '\0'; haystack++)
+	{
+		char *is = haystack;
+		char *pd = needle;
+
+		while (*is == *pd && *pd != '\0')
+		{
+			is++;
+			pd++;
+		}
+
+		if (*pd == '\0')
+		{
+			return (haystack);
+		}
+	}
+	return (0);
+}
