afd2db4f26debe1fec8eb263d382b332706d85b3
enghasssan <2200146@eng.asu.edu.eg>
2023-10-01 18:45:01 +0300

    power recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - get value of x^y
+ * @x: base number
+ * @y: pwr number
+ * Return: x^y or -1 if y < 0
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+		return (1);
+	if (y < 0)
+		return (-1);
+	return (x * _pow_recursion(x,y - 1));
+}
