bf3f53d4e20a1a79adccc939ca9807590fd9886b
enghasssan <2200146@eng.asu.edu.eg>
2023-10-06 01:43:53 +0300

    3-mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,18 +9,17 @@
 
 int main(int argc, char *argv[])
 {
-	int mul = 1;
+	int mul;
 
 	if (argc == 3)
 	{
 		mul = atoi(argv[argc - 2]) * atoi(argv[argc - 1]);
-	}
-	printf("%d\n", mul);
 	}
 	else
 	{
 		printf("Error\n");
 		return (1);
 	}
+	printf("%d\n", mul);
 	return (0);
 }
