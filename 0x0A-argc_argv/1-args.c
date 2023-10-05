02d9c6406b6dc98480fd859bf6ae8cb3a2f08be1
enghasssan <2200146@eng.asu.edu.eg>
2023-10-06 01:07:24 +0300

    args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -10,10 +10,10 @@
 int main(int argc, char *argv[] __attribute__((unused)))
 {
 	int i;
-	for (i = 1; i < argc; i++)
+	for (i = 0; i < argc; i++)
 	{
 		i++;
 	}
-	printf("%d\n", i);
+	printf("%d\n", i - 1);
 	return (0);
 }
