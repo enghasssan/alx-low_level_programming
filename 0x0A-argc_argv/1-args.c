717d95fff0316b76fd26083d9690a9d281841851
enghasssan <2200146@eng.asu.edu.eg>
2023-10-06 00:58:42 +0300

    args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,14 +4,12 @@
  * main - print the number of arguments
  * passed into the program
  * @argc: counter for size of array
- * @argv: array has strings
  * Return: 0
  */
 
 int main(int argc, char const *argv[])
 {
-	(void)argc;
-	int i = 0;
+	int i = 1;
 	while (argv[i] != '\0')
 	{
 		i++;
