cdc2ceeab96309dfa5c3331bbd16ff5e409de634
enghasssan <2200146@eng.asu.edu.eg>
2023-10-06 16:31:29 +0300

    adding inputs

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,7 +8,7 @@
  * Return: 0
  */
 
-int main(argc, *argv[])
+int main(int argc, char *argv[])
 {
 	char *c;
 	int i;
@@ -18,15 +18,15 @@ int main(argc, *argv[])
 	{
 		printf("0\n");
 	}
-	if (argc > 1)
+	else
 	{
 		for (i = 1; i < argc; i++)
 		{
 			c = argv[i];
+
 			if (*c < '0' || *c > '9')
 			{
-				printf("Error\n");
-				return (1);
+				return(printf("Error\n"), 1);
 			}
 			else
 				sum = sum + atoi(argv[i]);
